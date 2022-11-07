c74022a7573635b6d3a18f516bd9b829fec4b4cb
user <ujjalroy7862@gmail.com>
2022-11-07 13:41:11 +0600

    done this assignment successfull acroding to the instruction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Users from './components/Users';
 
 const App = () => {
   // step1 : declare three states here : users, isLoading, error
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error,setError] = useState(null)
+  
 
   // step2 : use useEffect for fetching the data including updating isLoading and error states
+
+  useEffect(() => {
+
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Data Fetching is not Successfull')
+      } else {
+        response.json()
+      }
+    })
+    .then(json => {
+      setData(json)
+      setIsLoading(false)
+      setError(null)
+    })
+    .catch((error) => {
+      setError(error.message)
+      setIsLoading(false)
+    })
+    
+  }, [])
+  
 
   return (
     <div className="container">
       <h1 className="title">Users Management App</h1>
       {isLoading && <p>Loading users...</p>}
       {error && <p>{error}</p>}
-      {/* step3 : pass the users data to Users component  */}
+      {Array.isArray(data) ? 
+        data.map((todo) => {
+          <Users key={Math.random} todo={todo.id}/>
+       }): null}
     </div>
   );
 };
